f39739614d318677674a50bbeb571b9e046a83da
Alex <phoenix.trade2307@gmail.com>
2021-11-28 14:28:35 +0200

    uncontrolled_accordion_button

--- src/components/UnControledAccordion/UncontroledAccordion.tsx
+++ src/components/UnControledAccordion/UncontroledAccordion.tsx
@@ -10,18 +10,18 @@ function UnControledAccordion(props: UnControledAccordionPropsType) {
     let [collapsed, setCollapsed] = useState(false)
 
         return <div>
-            <AccordionTitle title={props.titleValue}/>
-            <button onClick={ () =>  setCollapsed( !collapsed )}>TOGGLE</button>
+            <AccordionTitle title={props.titleValue} onClick={ () =>  setCollapsed( !collapsed ) }/>
             { !collapsed && <AccordionBody /> }
         </div>
 }
 
 type AccordionTitlePropsType = {
     title: string
+    onClick: () => void
 }
 function AccordionTitle(props: AccordionTitlePropsType) {
 
-    return <h3>-- {props.title} --</h3>
+    return <h3 onClick={ () => {props.onClick()} }>-- {props.title} --</h3>
 }
 
 function AccordionBody(props: any) {
